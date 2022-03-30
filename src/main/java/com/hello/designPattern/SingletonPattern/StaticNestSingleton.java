1a8bd32e13865fb14dec6d15e3334620c1afebd3
linmeng <1812323571@qq.com>
2022-03-30 16:18:57 +0800

    参考链接添加

--- src/main/java/com/hello/designPattern/SingletonPattern/StaticNestSingleton.java
+++ src/main/java/com/hello/designPattern/SingletonPattern/StaticNestSingleton.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
  * 静态内部类单例：外部类加载时不会加载内部类，只有在使用到内部类的时候虚拟机才会加载内部类
  *      保证了线程安全，单例的唯一性，延迟了类的实例化
  *      https://blog.csdn.net/mnb65482/article/details/80458571
+ *      https://juejin.cn/post/6871497501732519949
  * @author linmeng
  * @version 1.0
  * @date 2022年3月28日 17:00
