136973c053fa88910d9bda1fd89c1161557512d6
linmeng <1812323571@qq.com>
2022-03-31 15:43:22 +0800

    抽象工厂一些零碎文件提交

---
+++ src/main/java/com/hello/designPattern/FactoryPattern/abstractFactory/CarFactory.java
@@ -0,0 +1,4 @@
+package com.hello.designPattern.FactoryPattern.abstractFactory;
+
+public interface CarFactory {
+}
--- src/main/java/com/hello/designPattern/FactoryPattern/factory/TangCarFactory.java
+++ src/main/java/com/hello/designPattern/FactoryPattern/factory/TangCarFactory.java
@@ -1,13 +1,13 @@
 package com.hello.designPattern.FactoryPattern.factory;
 
 import com.hello.designPattern.FactoryPattern.interfaces.Car;
+import com.hello.designPattern.FactoryPattern.model.HighCar;
 import com.hello.designPattern.FactoryPattern.model.LowCar;
 import com.hello.designPattern.FactoryPattern.model.MiddleCar;
-import com.hello.designPattern.FactoryPattern.model.HighCar;
 
-public class QinCarFactory {
+public class TangCarFactory {
 
-    public static Car getCar(String carType) {
+    public static Car getCar(String carType) throws IllegalAccessException {
         switch (carType) {
             case "low":
                 return new LowCar();
@@ -16,7 +16,7 @@ public class QinCarFactory {
             case "high":
                 return new HighCar();
             default:
-                throw new IllegalArgumentException("param error");
+                throw new IllegalAccessException("param error");
         }
     }
 }
